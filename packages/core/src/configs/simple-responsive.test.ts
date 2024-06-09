6f41930bb8150bd34c185132fd291e532220d089
Florent Giraud <hello@florent.dev>
2024-06-09 12:01:41 -0400

    feat: connect compound to responsive

--- packages/core/src/configs/simple-responsive.test.ts
+++ packages/core/src/configs/simple-responsive.test.ts
@@ -24,25 +24,4 @@ describe('test resposive simple config', () => {
             })).toBe(twMerge(root_full_str))
         })
     })
-
-    // describe("with variants other than default", () => {
-    //     test("root", () => {
-    //         const root_full_str = "text-red-100 text-green-500"
-
-    //         expect(rootWithoutTwMerge({ color: "secondary" })).toBe(root_full_str)
-    //         expect(root({ color: "secondary" })).toBe(twMerge(root_full_str))
-    //     })
-    // })
-
-    // describe("with variants value not existing should not even take default", () => {
-    //     test("root", () => {
-    //         const root_full_str = "text-red-100"
-            
-    //         // @ts-expect-error
-    //         expect(rootWithoutTwMerge({ color: "tertiary" })).toBe(root_full_str)
-    //         // @ts-expect-error
-    //         expect(root({ color: "tertiary" })).toBe(twMerge(root_full_str))
-
-    //     })
-    // })
 })
--- packages/core/src/utils/compounds.ts
+++ packages/core/src/utils/compounds.ts
@@ -12,53 +12,39 @@ export const retrieveCompoundClasses = (compoundVariants: any, withDefaultProps:
     compoundVariants.forEach((compound: any) => {
 
         let hasFailed = false
+        let tmpClasses = new Set()
         
         Object.entries(compound.conditions).forEach(([key, value]: any) => {
             if (hasFailed) return
-            if (withDefaultProps[key] !== value) return hasFailed = true
+            if (typeof withDefaultProps[key] === "object") {
+                let found = false
+                let _k = ""
+                Object.entries(withDefaultProps[key]).forEach(([k, v]: any) => {
+                    if (value === v) {
+                        found = true
+                        _k = k
+                    }
+                })
+                if (!found) hasFailed = true
+                else {
+                    compound.class.split(" ").forEach((v: any) => {
+                        tmpClasses.add(`${_k}:${v}`)
+                    })
+                }
+            }
+            else if (withDefaultProps[key] !== value) return hasFailed = true
         })
 
         if (!hasFailed) {
-            const val = extractValue(compound.class, slot)
-            if (val !== " ") validCompounds.add(val)
+            if (tmpClasses.size > 0) {
+                tmpClasses.forEach(v => validCompounds.add(v))
+                tmpClasses.clear()
+            }
+            else { 
+                const val = extractValue(compound.class, slot)
+                if (val !== " ") validCompounds.add(val)
+            }
         } 
     })
-
-            // if (typeof withDefaultProps[key] == "undefined") {
-            //     if (otherProps[key] !== value) {
-            //         hasFailed = true
-            //     }
-            // } else if (typeof withDefaultProps[key] === "string") {
-            //     if (Array.isArray(value) && !value.includes(withDefaultProps[key])) hasFailed = true
-            //     else if (typeof value === "string" && value !== withDefaultProps[key]) hasFailed = true
-            // } else if (typeof withDefaultProps[key] === "object") {
-            //     let found = false
-            //     let _k = ""
-            //     Object.entries(withDefaultProps[key]).forEach(([k, v]: any) => {
-            //         if (value === v) {
-            //             found = true
-            //             _k = k
-            //         }
-            //     })
-            //     if (!found) hasFailed = true
-            //     else {
-            //         compound.class.split(" ").forEach((v: any) => {
-            //             tmpClasses.push(`${_k}:${v}`)
-            //         })
-            //     }
-            // } else if (typeof withDefaultProps[key] === "boolean") {
-            //     if (value !== withDefaultProps[key]) hasFailed = true
-            //     else {
-            //         if (typeof compound.class === "string") tmpClasses.push(compound.class)
-            //         else compound.class[slot] ? tmpClasses.push(compound.class[slot]) : ""
-            //     }
-            // }
-    //     })
-
-    //     if (tmpClasses.size > 1) tmpClasses.forEach(v => validCompounds.add(v))
-        // if (!hasFailed) tmpClasses.forEach(v => {
-        //     validCompounds.add(v)
-        // })
-    // })
     return Array.from(validCompounds)
 }

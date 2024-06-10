0c2bec2118d41516e29733ae6f6901c1fbcffce1
florent giraud <hello@florent.dev>
2024-06-10 01:26:54 -0400

    chore: try documentation deployment

--- packages/documentation/nuxt.config.ts
+++ packages/documentation/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   extends: ['shadcn-docs-nuxt'],
+  telemetry: false,
 });

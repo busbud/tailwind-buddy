a3e077f5c030d9855ce11db170eb33ea7d393b45
florent giraud <hello@florent.dev>
2024-09-25 10:39:32 -0400

    feat(twmerge): inject function like tailwind merge possible (#25)
    * feat(twmerge): inject function like tailwind merge possible

--- packages/ui/src/tailwind-buddy-interface.ts
+++ packages/ui/src/tailwind-buddy-interface.ts
@@ -1,5 +1,6 @@
 import { setupCompose } from "@busbud/tailwind-buddy";
+import { twMerge } from "tailwind-merge";
 
 export type Screens = "sm" | "md";
 export const screens: Screens[] = ["sm", "md"];
-export const compose = setupCompose<Screens>(screens);
+export const compose = setupCompose<Screens>(screens, twMerge);
